0e45ce78b13426d062adc80c52d325deb2c3973d
Cina Shi <ccc1nnn4@gmail.com>
2019-03-04 17:56:13 -0500

    data noun count updated: USE PYTHON3

--- extractNouns.py
+++ extractNouns.py
@@ -1,5 +1,4 @@
 import nltk
-import string
 
 resultf = open("counts.txt", 'w')
 for i in range(10):
@@ -8,14 +7,8 @@ for i in range(10):
 	outputFilename = "noun_counting_data/a" + str(i+1) + "-nouns.txt"
 	of = open(outputFilename, 'w')
 	with open(filename, 'r') as f:
-		content = f.readlines()
-		content = [x.strip() for x in content]
-		# function to test if something is a noun
 		is_noun = lambda pos: pos[:2] == 'NN'
-		# do the nlp stuff
-		content = " ".join(content)
-		printable = set(string.printable)
-		content = filter(lambda x: x in printable, content)
+		content = (" ".join(line.strip() for line in f))
 		tokenized = nltk.word_tokenize(content)
 		nouns = [word for (word, pos) in nltk.pos_tag(tokenized) if is_noun(pos)]
 		resultf.write("a" + str(i+1) + " noun count: " + str(len(nouns)) + "\n")
